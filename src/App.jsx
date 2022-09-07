f2fb98af22c3569afc26efc52c204c4edd87409e
Nisal <nsltharaka@gmail.com>
2022-09-07 11:31:31 +0530

    hero section - desktop done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 export default function App() {
 
   return (
-    <div>
+    <div className="container">
       <Header />
+      <Hero />
     </div>
   );
 }
