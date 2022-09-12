9e7b32bdfae1dffbb933ad9e05a21d4d0aaec3bc
Nisal <nsltharaka@gmail.com>
2022-09-12 21:03:47 +0530

    section about us done

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
 import Hero from "./components/home/Hero";
 import AboutUs from "./components/home/AboutUs";
 import './styles/home.css';
+import Feedback from "./components/home/Feedbacks";
 
 export default function Home() {
 
@@ -8,6 +9,7 @@ export default function Home() {
         <>
             <Hero />
             <AboutUs />
+            <Feedback />
         </>
     )
 
