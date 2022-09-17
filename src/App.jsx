ef6e6544a949430e19c3104227ab9cdadf57c7aa
Nisal <nsltharaka@gmail.com>
2022-09-17 17:11:10 +0530

    feedback done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { AppContextProvider } from "./context";
 import Home from "./Home";
@@ -9,6 +10,7 @@ export default function App() {
     <AppContextProvider>
       <Header />
       <Home />
+      <Footer />
     </AppContextProvider>
   );
 }
