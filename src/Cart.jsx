75788079ef639088669fb5c0b580d8ff9132cf5c
Nisal <nsltharaka@gmail.com>
2022-11-15 11:12:47 +0530

    cart done

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,6 +1,8 @@
 import CartItem from "./components/Cart/CartItem"
 import "./styles/Cart.css"
 import useAppContext from "./useAppContext"
+import { Link } from "react-router-dom"
+import { HiOutlineShoppingBag } from "react-icons/hi"
 
 export default function Cart() {
 
@@ -16,8 +18,26 @@ export default function Cart() {
     />)
 
     return (
-        <div className="cart">
-            {components}
-        </div>
+        cartItems.length > 0 ?
+            <div className="cart">
+                {components}
+                <div className="subTotal">
+                    <h4> Sub Total <br /> LKR {cartItems
+                        .map(i => i.price * i.qty)
+                        .reduce((acc = 0, x) => acc + x)}.00</h4>
+                </div>
+                <div className="cart-buttons">
+                    <button className="cart-btn placeOrder">Place Order</button>
+                    <button className="cart-btn continue"><Link to="/products">Continue Shopping</Link></button>
+                </div>
+            </div>
+            :
+            <div className="emptyCart">
+                <HiOutlineShoppingBag className="emptyCart-icon" />
+                <p>your cart is empty</p>
+                <Link to="/products">
+                    <button className="btn-continueShopping">Continue Shopping</button>
+                </Link>
+            </div>
     )
 }
