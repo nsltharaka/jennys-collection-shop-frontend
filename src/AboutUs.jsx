3c73cb9cdae154b5502a1f5670982ea77f259461
Nisal <nsltharaka@gmail.com>
2022-11-15 12:41:40 +0530

    about us page mission and vision removed

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,13 +1,11 @@
 import Location from "./components/aboutUs/Location";
 import OwnerInfo from "./components/aboutUs/OwnerInfo";
-import Vision from "./components/aboutUs/Vision";
 import "./styles/AboutUs.css";
 
 export default function AboutUs() {
     return (
         <>
             <OwnerInfo />
-            <Vision />
             <Location />
         </>
     )
--- src/components/aboutUs/OwnerInfo.jsx
+++ src/components/aboutUs/OwnerInfo.jsx
@@ -8,7 +8,10 @@ export default function OwnerInfo() {
             <div className="ownerInfo-text">
                 <h1 className="ownerInfo-text--title">Meet Jenny!</h1>
                 <p className="ownerInfo-text--desc">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos labore similique repellendus accusamus qui? Labore aperiam quo alias ducimus rem hic veniam, expedita veritatis praesentium sapiente maxime nobis voluptatum est?
+                    Hi! My name is Jenny and I started Jenny's Collection in 2018 as a side hustle. Since that time, the business has grown to include a shop and now, an online shop!
+                    <br />
+                    <br />
+                    My commitment to you is that my products will always be made in Sri Lanka using high quality fabrics, so they're made to last.
                 </p>
             </div>
         </section>
